33aa4bb42a3bc810fed236fba9c77c9da907fab7
Ale6875 <kovacevicaleksandar55@gmail.com>
2025-01-15 11:33:33 +0100

    Add files via upload

---
+++ js/sweetAlert.js
@@ -0,0 +1,7 @@
+document.getElementById("menuIcon").onclick = function() {
+    Swal.fire({
+        title: "Vizitka",
+        html: "Aleksandar Kovačević, 4. Ra <br> ERŠ Nova Gorica",
+        icon: "info"
+    });
+};
